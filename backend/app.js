737e3923f4709b1fa478c4877e9d1e8761bf62b4
Mohit Passan <mohit.p@freestand.in>
2022-03-20 15:14:20 +0530

    Backend connected to NotionDB

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const app = express();
 
+const dotenv = require('dotenv');
+dotenv.config();
+
+const { Client } = require('@notionhq/client');
+const notion = new Client({ auth: process.env.API_KEY });
+
 app.get('/', (req, res) => {
-    res.status(200).send("Working");
+    const response = notion.databases.query({
+        database_id: process.env.DB_ID,
+    });
+
+    response.then((result) => {
+        const tasks = result.results;
+        res.status(200).json(tasks.map(task => ({
+            task: task.properties.Task.title[0].text.content,
+            status: task.properties.Status.checkbox
+        })));
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).send("Internal server error");
+    })
 });
 
 app.listen(5000, () => {
