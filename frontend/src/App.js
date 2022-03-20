94a1dbcc78f986d1aa87b73e8a9151424d925520
Mohit Passan <mohit.p@freestand.in>
2022-03-20 15:34:35 +0530

    Frontend getting tasks from the backend successfully

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,43 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const tasksResponse = axios.get('http://localhost:5000/');
+    
+    tasksResponse
+      .then((result) => {
+        setTasks(result.data);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+  }, [tasks.length, error]);
+
   return (
     <div className="App">
-      
+      {
+        !error ? (
+          <>
+            {
+              tasks.length !== 0 ? (
+                <ul>
+                  {
+                    tasks.map((task) => <li key={task.task}>{task.task}: {task.status ? "Done" : "Pending"}</li>)
+                  }
+                </ul>
+              ) : (
+                <h2>No tasks</h2>
+              )
+            }
+          </>
+        ) : (
+          <h1>Error aagya</h1>
+        )
+      }
     </div>
   );
 }
