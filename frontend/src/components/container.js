18a7f57460cbe48632273db8f6aabe3e09269613
Mohit Passan <mohit.p@freestand.in>
2022-04-01 23:47:55 +0530

    Introduced breakpoints and changed grid according to them

--- frontend/src/components/container.js
+++ frontend/src/components/container.js
@@ -6,21 +6,43 @@ const Container = styled.div`
     box-sizing: border-box;
 
     display: grid;
-    grid-template-rows: repeat(3, 1fr);
-    grid-template-columns: repeat(12, 1fr);
+    grid-template-rows: repeat(5, 1fr);
+    grid-template-columns: repeat(4, 1fr);
     grid-gap: 20px;
 
     grid-template-areas: 
-        "cc cc cc cc cc cc cc b b b b b"
-        "c1 c1 c1 c1 c1 c1 c1 c2 c2 c2 c2 c2"
-        "c3 c3 c3 c3 c3 c3 c3 c4 c4 c4 c4 c4";
+        "cc cc b b"
+        "c1 c1 c1 c1"
+        "c2 c2 c2 c2"
+        "c3 c3 c3 c3"
+        "c4 c4 c4 c4";
 
-    @media (min-width: 768px) {
+    @media (min-width: 624px) {
         padding: 0 40px;
+
+        grid-template-rows: repeat(4, 1fr);
+        grid-template-columns: repeat(8, 1fr);
+        grid-gap: 20px;
+
+        grid-template-areas: 
+            "cc cc cc cc cc b b b"
+            "c1 c1 c1 c1 c1 c1 c1 c1" 
+            "c2 c2 c2 c2 c3 c3 c3 c3"
+            "c4 c4 c4 c4 c4 c4 c4 c4";
     }
 
-    @media (min-width: 992px) {
+    @media (min-width: 923px) {
         padding: 0 70px;
+
+        display: grid;
+        grid-template-rows: repeat(3, 1fr);
+        grid-template-columns: repeat(12, 1fr);
+        grid-gap: 20px;
+
+        grid-template-areas: 
+            "cc cc cc cc cc cc cc b b b b b"
+            "c1 c1 c1 c1 c1 c1 c1 c2 c2 c2 c2 c2"
+            "c3 c3 c3 c3 c3 c3 c3 c4 c4 c4 c4 c4";
     }
 `
 
