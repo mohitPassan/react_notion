6f2e26ab9882add7b871dbde9ca325c9f473c90b
Mohit Passan <mohit.p@freestand.in>
2022-04-01 18:56:06 +0530

    Structured the incoming data and returning the new structure

--- backend/controllers/expenses.controllers.js
+++ backend/controllers/expenses.controllers.js
@@ -4,7 +4,31 @@ const getExpenses = (req, res) => {
     let expensesResponse = expensesModel.getExpenses();
 
     expensesResponse.then((result) => {
-        res.status(200).json(result);
+        const finalData = result.results.map(item => ({
+            id: item.id,
+            amount: {
+                value: item.properties.Amount.number
+            },
+            category: {
+                value: item.properties.Category.select.name,
+                color: item.properties.Category.select.color
+            },
+            date: {
+                value: item.properties.Date.date.start
+            },
+            description: {
+                value: item.properties.Description.title[0].text.content
+            },
+            modeOfPayment: {
+                value: item.properties["Mode of payment"].select.name,
+                color: item.properties["Mode of payment"].select.color
+            },
+            type: {
+                value: item.properties.Type.select.name,
+            }
+        }));
+        
+        res.status(200).json(finalData);
     })
     .catch((err) => {
         console.log(err);
